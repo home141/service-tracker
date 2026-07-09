b1bbbbe15f24a19924c78d6c3c6026d61d0522ab
akkyzero <104045564+$akkyzero@users.noreply.github.com>
2026-07-09 09:59:26 +0000

    chore: update Git configuration system in Dev Container

---
+++ .devcontainer/scripts/on-create.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.config/git
+cp /tmp/git-config/config-* ~/.config/git/
--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
 sudo chown -R vscode:vscode .
-
-git config set gpg.ssh.program ssh-keygen
